3bbea772ccdf8eba2d80d18952761b1fa106a156
rxluz <rx@appock.co>
2017-09-15 21:45:47 +0100

    node server 1

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var server = express();
 server.use(evh.vhost(server.enabled('trust proxy')));
 server.listen(3000);
 
-evh.register('api.infestus.cc', require('./apps/infestus.api/app.js').app);
+evh.register('api.infestus.cc', require('./apps/infestus.api/app.js'));
 var app2 = appFactory('test2');
 evh.register('apinew.infestus.cc', app2);
 //evh.register('*.test2-local', appFactory('test2'));
